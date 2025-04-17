06edae9401998a09fa944fd2cf52993acf261bc0
cmnguyen252 <cmnguyen252@gmail.com>
2025-04-16 21:18:30 -0500

    needed to add provider

--- DiscordBot/discord.tf
+++ DiscordBot/discord.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.93"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "Discord" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t2.micro"
