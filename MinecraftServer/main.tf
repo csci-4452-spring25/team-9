5c3401187d4d05863a7fe9a09a438e4ccaa215fe
cmnguyen252 <cmnguyen252@gmail.com>
2025-04-23 14:29:46 -0500

    fix to service

--- MinecraftServer/main.tf
+++ MinecraftServer/main.tf
@@ -22,23 +22,29 @@ resource "aws_instance" "minecraft" {
   user_data              = <<-EOF
     #!/bin/bash
     MINECRAFTSERVERURL=${var.mojang_server_url}
+    
     sudo yum install -y java-21-amazon-corretto-headless
+    
     adduser minecraft
     mkdir /opt/minecraft/
     mkdir /opt/minecraft/server/
     cd /opt/minecraft/server
+
     wget $MINECRAFTSERVERURL
+
+    chown -R minecraft:minecraft /opt/minecraft
     java -Xmx1300M -Xms1300M -jar server.jar nogui
     sleep 40
     sed -i 's/false/true/p' eula.txt
     touch start
-    printf '#!/bin/bash\ncd /opt/minecraft/server\nsudo java -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
+    printf '#!/bin/bash\nsudo java -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
     chmod +x start
     sleep 1
     touch stop
-    printf '#!/bin/bash\ncd /opt/minecraft/server\nsudo kill -9 $(ps -ef | pgrep -f "java")' >> stop
+    printf '#!/bin/bash\nsudo kill -9 $(ps -ef | pgrep -f "java")' >> stop
     chmod +x stop
     sleep 1
+    
     cd /etc/systemd/system
     touch minecraft.service
     printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
